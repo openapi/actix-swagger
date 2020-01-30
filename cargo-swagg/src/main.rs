a7d7c8ef88bae1ebbe77f2900b37898b7881135d
Sergey Sova <mail@sergeysova.com>
2020-01-30 22:28:56 +0300

    feat(cargo-swagg): add bind method generation

--- cargo-swagg/src/main.rs
+++ cargo-swagg/src/main.rs
@@ -20,70 +20,36 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     //     }
     // };
 
-    let tokens = api.info.print();
+    let api: ApiStruct = api.info.into();
 
-    println!("{}", tokens);
+    println!("{}", api.print());
+
+    let m1 = BindApiMethod {
+        method: HttpMethod::Get,
+        name: "sessionGet".to_owned(),
+        path: "/session".to_owned(),
+        response_type: "sessionGetResponse".to_owned(),
+    };
+
+    let m2 = BindApiMethod {
+        method: HttpMethod::Post,
+        name: "sessionCreate".to_owned(),
+        path: "/session".to_owned(),
+        response_type: "sessionCreateResponse".to_owned(),
+    };
+
+    let imple = ImplApiMethods {
+        api_name: api.api_name.clone(),
+        methods: vec![m1, m2],
+    };
+
+    println!("{}", imple.print());
 
     Ok(())
 }
 
 trait Printable {
     fn print(&self) -> proc_macro2::TokenStream;
-}
-
-impl Printable for openapiv3::Info {
-    fn print(&self) -> proc_macro2::TokenStream {
-        let api_name = format_ident!("{}", to_struct_name(self.title.to_owned()));
-        let terms = self
-            .terms_of_service
-            .to_owned()
-            .map_or(String::default(), |terms| format!("@see {}", terms));
-        let description = self.description.to_owned().unwrap_or_default();
-
-        let doc_comment = format!("{}\n{}", description, terms);
-
-        quote! {
-            #[doc = #doc_comment]
-            pub struct #api_name {
-                root: actix_web::Scope,
-                routes: std::collections::HashMap<String, Scope>
-            }
-
-            impl #api_name {
-                pub fn new() -> Self {
-                    Self {
-                        root: actix_web::Scope:: new("/"),
-                        routes: std::collections::HashMap::new(),
-                    }
-                }
-            }
-
-            impl actix_web::dev::HttpServiceFactory for #api_name {
-                fn register(mut self, config: &mut actix_web::dev::AppService) {
-                    let keys: Vec<String> = self.routes.keys().cloned().collect();
-
-                    for key in keys.iter() {
-                        if let Some(route) = self.routes.remove(key) {
-                            self.root = self.root.service(resource);
-                        }
-                    }
-
-                    self.root.register(config);
-                }
-            }
-        }
-    }
-}
-
-impl Printable for OpenAPI {
-    fn print(&self) -> proc_macro2::TokenStream {
-        let api_struct = self.info.print();
-        // let api_name = format_ident!("{}", to_struct_name(self.info.title.to_owned()));
-
-        quote! {
-            #api_struct
-        }
-    }
 }
 
 /// Create PascalName from string
@@ -93,4 +59,148 @@ fn to_struct_name(string: String) -> String {
     re_name
         .replace_all(string.to_pascal_case().as_ref(), "")
         .to_string()
+}
+
+/// Object describing main api structure and useful impls
+pub struct ApiStruct {
+    pub(crate) api_name: String,
+    pub(crate) terms_of_service: Option<String>,
+    pub(crate) description: Option<String>,
+}
+
+impl ApiStruct {
+    pub fn new(api_name: String) -> Self {
+        Self {
+            api_name,
+            terms_of_service: None,
+            description: None,
+        }
+    }
+}
+
+impl From<openapiv3::Info> for ApiStruct {
+    fn from(info: openapiv3::Info) -> Self {
+        Self {
+            api_name: to_struct_name(info.title),
+            description: info.description,
+            terms_of_service: info.terms_of_service,
+        }
+    }
+}
+
+impl Printable for ApiStruct {
+    fn print(&self) -> proc_macro2::TokenStream {
+        let api_name = format_ident!("{}", to_struct_name(self.api_name.to_owned()));
+        let terms = self
+            .terms_of_service
+            .to_owned()
+            .map_or(String::default(), |terms| format!("@see {}", terms));
+        let description = self.description.to_owned().unwrap_or_default();
+
+        let doc_comment = format!("{}\n{}", description, terms);
+        let doc = doc_comment.trim();
+
+        quote! {
+            #[doc = #doc]
+            pub struct #api_name {
+                api: actix_swagger::Api,
+            }
+
+            impl #api_name {
+                pub fn new() -> Self {
+                    Self {
+                        api: actix_swagger::Api::new()
+                    }
+                }
+            }
+
+            impl Default for #api_name {
+                fn default() -> Self {
+                    let api = Self::new();
+                    api
+                }
+            }
+
+            impl actix_web::dev::HttpServiceFactory for #api_name {
+                fn register(mut self, config: &mut actix_web::dev::AppService) {
+                    self.api.register(config);
+                }
+            }
+        }
+    }
+}
+
+enum HttpMethod {
+    Get,
+    Post,
+    Patch,
+    Put,
+    Delete,
+}
+
+impl ToString for HttpMethod {
+    fn to_string(&self) -> String {
+        match self {
+            HttpMethod::Get => "GET",
+            HttpMethod::Post => "POST",
+            HttpMethod::Put => "PUT",
+            HttpMethod::Patch => "PATCH",
+            HttpMethod::Delete => "DELETE",
+        }
+        .to_owned()
+    }
+}
+
+struct BindApiMethod {
+    pub(crate) method: HttpMethod,
+    pub(crate) path: String,
+    pub(crate) name: String,
+    pub(crate) response_type: String,
+}
+
+impl Printable for BindApiMethod {
+    fn print(&self) -> proc_macro2::TokenStream {
+        let request_path = self.path.clone();
+        let http_method = format_ident!("{}", self.method.to_string());
+        let response_type = format_ident!("{}", self.response_type.to_pascal_case());
+        let bind_method_name = format_ident!("bind_{}", self.name.to_snake_case());
+
+        quote! {
+            pub fn #bind_method_name<F, T, R>(mut self, handler: F) -> Self
+            where
+                F: actix_web::dev::Factory<T, R, actix_swagger::Answer<'static, super::paths::#response_type>>,
+                T: actix_web::FromRequest + 'static,
+                R: std::future::Future<Output = actix_swagger::Answer<'static, super::paths::#response_type>> + 'static,
+            {
+                self.api = self.api.bind(#request_path.to_owned(), actix_web::http::Method::#http_method, handler);
+                self
+            }
+        }
+    }
+}
+
+struct ImplApiMethods {
+    api_name: String,
+    methods: Vec<BindApiMethod>,
+}
+
+impl Printable for ImplApiMethods {
+    fn print(&self) -> proc_macro2::TokenStream {
+        let api_name = format_ident!("{}", to_struct_name(self.api_name.to_owned()));
+        let mut tokens = quote! {};
+
+        for method in &self.methods {
+            let method_tokens = method.print();
+            tokens = quote! {
+                #tokens
+
+                #method_tokens
+            };
+        }
+        quote! {
+            impl #api_name {
+                #tokens
+            }
+        }
+    }
 }
