572f0e8fe60e12da43e33684bd9ed0b450cf5a85
Sergey Sova <s.sova@redmadrobot.com>
2020-05-07 19:16:29 +0300

    feat(swagg): trying to parse schema object properties

--- swagg/src/highway/mod.rs
+++ swagg/src/highway/mod.rs
@@ -10,6 +10,7 @@ use openapiv3::ReferenceOr;
 #[derive(Debug, Default)]
 pub struct Components {
     pub request_bodies: IndexMap<String, RequestBody>,
+    pub schemas: IndexMap<String, Component>,
 }
 
 #[derive(Debug, Default, PartialEq, Eq)]
@@ -24,10 +25,30 @@ pub struct RequestBody {
 pub struct Component {
     pub name: String,
     pub description: Option<String>,
-    pub fields: IndexMap<String, ComponentField>,
+    pub kind: ComponentKind,
 }
 
-#[derive(Debug, Default, Hash, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum ComponentKind {
+    Object {
+        fields: IndexMap<String, ComponentField>,
+    },
+    Array {
+        items: FieldType,
+    },
+    Integer,
+    String,
+    Number,
+    Boolean,
+}
+
+impl Default for ComponentKind {
+    fn default() -> Self {
+        Self::String
+    }
+}
+
+#[derive(Debug, Default, Hash, PartialEq, Eq, Clone)]
 pub struct ComponentField {
     pub name: String,
     pub required: bool,
@@ -35,9 +56,15 @@ pub struct ComponentField {
     pub field_type: FieldType,
 }
 
-#[derive(Debug, Hash, PartialEq, Eq)]
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
 pub enum FieldType {
     String,
+    Number,
+    Integer,
+    Boolean,
+
+    /// Name of the type in module
+    Type(String),
 }
 
 impl Default for FieldType {
@@ -55,92 +82,111 @@ impl Components {
         self.request_bodies.insert(body.name.clone(), body);
     }
 
-    /// Parse request body from openapiv3 and add it to index
-    pub fn parse_request_body(&mut self, name: &String, request_body: &openapiv3::RequestBody) {
-        log::trace!("Parsing request body {}", name);
-        let mut highway_body = RequestBody::default();
+    // Parse request body from openapiv3 and add it to index
+    // pub fn parse_request_body(&mut self, name: &String, request_body: &openapiv3::RequestBody) {
+    //     log::trace!("Parsing request body {}", name);
+    //     let mut highway_body = RequestBody::default();
 
-        highway_body.name = name.clone();
-        highway_body.description = request_body.description.clone();
-        highway_body.required = request_body.required;
+    //     highway_body.name = name.clone();
+    //     highway_body.description = request_body.description.clone();
+    //     highway_body.required = request_body.required;
 
-        if let Some(media_type) = request_body.content.get("application/json") {
-            if let Some(ReferenceOr::Item(schema)) = media_type.schema.clone() {
-                self.parse_body_schema(&schema);
-            } else {
-                log::info!(
-                    "skipping media type {:?} in {}.content, supported only inline schema",
-                    media_type.schema,
-                    name
-                );
-            }
-        } else {
-            log::info!(
-                "skipping content {:?} in {}, supported only application/json",
-                request_body.content,
-                name
-            );
-        }
+    //     if let Some(media_type) = request_body.content.get("application/json") {
+    //         if let Some(ReferenceOr::Item(schema)) = media_type.schema.clone() {
+    //             self.parse_body_schema(&schema);
+    //         } else {
+    //             log::info!(
+    //                 "skipping media type {:?} in {}.content, supported only inline schema",
+    //                 media_type.schema,
+    //                 name
+    //             );
+    //         }
+    //     } else {
+    //         log::info!(
+    //             "skipping content {:?} in {}, supported only application/json",
+    //             request_body.content,
+    //             name
+    //         );
+    //     }
 
-        self.insert_request_body(highway_body);
-    }
+    //     self.insert_request_body(highway_body);
+    // }
 
-    fn parse_body_schema(&mut self, schema: &openapiv3::Schema) {
-        log::trace!("Parsing schema {:?}", schema);
-        use openapiv3::{SchemaKind, Type};
+    // fn parse_body_schema(&mut self, schema: &openapiv3::Schema) {
+    //     log::trace!("Parsing schema {:?}", schema);
+    //     use openapiv3::{SchemaKind, Type};
 
-        match schema.schema_kind.clone() {
-            SchemaKind::Type(internal_type) => match internal_type {
-                Type::Object(object) => {
-                    let component = Self::parse_object_to_component(&object);
-                    let body = RequestBody {
-                        name: component.name,
-                        description: component.description,
-                        required: true,
-                        fields: component.fields,
-                    };
-                    self.insert_request_body(body);
-                }
-                unsupported_type => {
-                    log::info!("unsupported schema type {:?}", unsupported_type);
-                }
-            },
-            unsupported_kind => {
-                log::info!("unsupported schema kind {:?}", unsupported_kind);
-            }
-        }
-    }
+    //     match schema.schema_kind.clone() {
+    //         SchemaKind::Type(internal_type) => match internal_type {
+    //             Type::Object(object) => {
+    //                 let component = Self::parse_object_to_component(&object);
+    //                 let body = RequestBody {
+    //                     name: component.name,
+    //                     description: component.description,
+    //                     required: true,
+    //                     fields: component.fields,
+    //                 };
+    //                 self.insert_request_body(body);
+    //             }
+    //             unsupported_type => {
+    //                 log::info!("unsupported schema type {:?}", unsupported_type);
+    //             }
+    //         },
+    //         unsupported_kind => {
+    //             log::info!("unsupported schema kind {:?}", unsupported_kind);
+    //         }
+    //     }
+    // }
 
-    /// Can't parse description and name
-    fn parse_object_to_component(object: &openapiv3::ObjectType) -> Component {
-        use openapiv3::{SchemaKind, Type};
-        let mut component = Component::default();
+    // Can't parse description and name
+    // fn parse_object_to_component(object: &openapiv3::ObjectType) -> Component {
+    //     use openapiv3::{SchemaKind, Type};
+    //     let mut component = Component::default();
 
-        for (name, schema) in object.properties.iter() {
-            let required = object.required.iter().find(|e| *e == name).is_some();
+    //     for (name, schema) in object.properties.iter() {
+    //         let required = object.required.iter().find(|e| *e == name).is_some();
 
-            if let ReferenceOr::Item(schema) = schema.clone().unbox() {
-                match schema.schema_kind {
-                    SchemaKind::Type(Type::String(_string_type)) => {
-                        let field_type = FieldType::String;
-                        let field = ComponentField {
-                            name: name.clone(),
-                            description: schema.schema_data.description.clone(),
-                            required,
-                            field_type,
-                        };
-                        component.fields.insert(name.clone(), field);
-                    }
-                    unsupported_kind => {
-                        log::info!("skipping unsupported kind {:?}", unsupported_kind);
-                        continue;
-                    }
-                }
-            } else {
-                log::info!("skipping property {} due to unsupported refs", name);
-            }
-        }
+    //         if let ReferenceOr::Item(schema) = schema.clone().unbox() {
+    //             match schema.schema_kind {
+    //                 SchemaKind::Type(Type::String(_string_type)) => {
+    //                     let field_type = FieldType::String;
+    //                     let field = ComponentField {
+    //                         name: name.clone(),
+    //                         description: schema.schema_data.description.clone(),
+    //                         required,
+    //                         field_type,
+    //                     };
+    //                     component.fields.insert(name.clone(), field);
+    //                 }
+    //                 unsupported_kind => {
+    //                     log::info!("skipping unsupported kind {:?}", unsupported_kind);
+    //                     continue;
+    //                 }
+    //             }
+    //         } else {
+    //             log::info!("skipping property {} due to unsupported refs", name);
+    //         }
+    //     }
 
-        component
-    }
+    //     component
+    // }
+}
+
+pub enum Reference {
+    /// References to the current file
+    Relative(ReferenceRelative),
+
+    /// References to the file on the current disk
+    File(String),
+
+    /// References to the file in the web
+    Remote,
+}
+
+pub enum ReferenceRelative {
+    Response { name: String },
+    Parameter { name: String },
+    RequestBody { name: String },
+    Schema { name: String },
+    Responses { name: String },
 }
--- swagg/src/lib.rs
+++ swagg/src/lib.rs
@@ -49,19 +49,180 @@ pub fn to_string(source: &str, format: Format) -> Result<String, Error> {
         .set_terms_of_service(api.info.terms_of_service);
 
     if let Some(components) = api.components {
-        for (name, body) in components.request_bodies.iter() {
-            match body {
-                ReferenceOr::Item(body) => {
-                    highway_components.parse_request_body(&name, &body);
-                }
-                ReferenceOr::Reference { reference } => {
-                    log::info!("skipping request body reference {}", reference);
-                }
-            }
+        // for (name, body) in components.request_bodies.iter() {
+        //     match body {
+        //         ReferenceOr::Item(body) => {
+        //             highway_components.parse_request_body(&name, &body);
+        //         }
+        //         ReferenceOr::Reference { reference } => {
+        //             log::info!("skipping request body reference {}", reference);
+        //         }
+        //     }
+        // }
+
+        println!("{:#?}", components.schemas);
+
+        for (name, schema) in components.schemas.iter() {
+            parse_ref_or_schema(&name, &schema);
         }
     }
 
     println!("{:#?}", highway_components);
 
     Ok(format!("{}", generated.print()))
+}
+
+fn parse_ref_or_schema(
+    name: &String,
+    ref_or: &ReferenceOr<openapiv3::Schema>,
+) -> Vec<highway::Component> {
+    match ref_or {
+        ReferenceOr::Item(schema) => {
+            let comp = parse_schema(&name, &schema);
+
+            println!("{:#?}", comp);
+
+            comp
+        }
+        ReferenceOr::Reference { reference } => {
+            log::info!(
+                "reference for schemas is not supported yet. Skipping {} for {}...",
+                reference,
+                name
+            );
+
+            vec![]
+        }
+    }
+}
+
+fn parse_schema(name: &String, schema: &openapiv3::Schema) -> Vec<highway::Component> {
+    use inflections::Inflect;
+
+    let mut list = vec![];
+
+    let mut component = highway::Component {
+        name: name.clone().to_pascal_case(),
+        description: schema.schema_data.description.clone(),
+        ..Default::default()
+    };
+
+    use openapiv3::{SchemaKind, Type};
+
+    match &schema.schema_kind {
+        SchemaKind::Type(schema_type) => {
+            let kind: highway::ComponentKind = match schema_type {
+                Type::Object(object) => {
+                    let mut fields = indexmap::IndexMap::new();
+
+                    // Iterate over fields in object
+                    for (field, schema) in object.properties.iter() {
+                        // Parse field schema to highway components
+                        let field_components = parse_ref_or_schema(&field, &schema.clone().unbox());
+
+                        for field_component in field_components.iter() {
+                            let required = object
+                                .required
+                                .iter()
+                                .find(|found| *found == field)
+                                .is_some();
+
+                            let description = field_component.description.clone();
+
+                            match field_component.kind {
+                                highway::ComponentKind::Number => {
+                                    fields.insert(
+                                        field.clone(),
+                                        highway::ComponentField {
+                                            name: field.clone(),
+                                            required,
+                                            description,
+                                            field_type: highway::FieldType::Number,
+                                        },
+                                    );
+                                }
+                                highway::ComponentKind::Integer => {
+                                    fields.insert(
+                                        field.clone(),
+                                        highway::ComponentField {
+                                            name: field.clone(),
+                                            required,
+                                            description,
+                                            field_type: highway::FieldType::Integer,
+                                        },
+                                    );
+                                }
+                                highway::ComponentKind::Boolean => {
+                                    fields.insert(
+                                        field.clone(),
+                                        highway::ComponentField {
+                                            name: field.clone(),
+                                            required,
+                                            description,
+                                            field_type: highway::FieldType::Boolean,
+                                        },
+                                    );
+                                }
+                                highway::ComponentKind::String => {
+                                    fields.insert(
+                                        field.clone(),
+                                        highway::ComponentField {
+                                            name: field.clone(),
+                                            required,
+                                            description,
+                                            field_type: highway::FieldType::String,
+                                        },
+                                    );
+                                }
+                                highway::ComponentKind::Object { .. } => {
+                                    let field_structure_component = highway::Component {
+                                        // Full name of the structure for nested object
+                                        name: format!(
+                                            "{}{}",
+                                            name.to_pascal_case(),
+                                            field.to_pascal_case()
+                                        ),
+                                        description: field_component.description.clone(),
+                                        kind: field_component.kind.clone(),
+                                    };
+
+                                    fields.insert(
+                                        field.clone(),
+                                        highway::ComponentField {
+                                            /// name of the field
+                                            name: field.clone(),
+                                            required,
+                                            description,
+                                            field_type: highway::FieldType::Type(
+                                                field_structure_component.name.clone(),
+                                            ),
+                                        },
+                                    );
+
+                                    list.push(field_structure_component);
+                                }
+                                _ => {}
+                            };
+                        }
+                    }
+
+                    highway::ComponentKind::Object { fields }
+                }
+                Type::Array(array) => highway::ComponentKind::Array {
+                    items: Default::default(),
+                },
+                Type::Number(_number) => highway::ComponentKind::Number,
+                Type::Integer(_integer) => highway::ComponentKind::Integer,
+                Type::String(_string) => highway::ComponentKind::String,
+                Type::Boolean {} => highway::ComponentKind::Boolean,
+            };
+
+            component.kind = kind;
+        }
+        other => log::info!("this schema kind is not supported {:?}", other),
+    }
+
+    list.push(component);
+
+    list
 }
