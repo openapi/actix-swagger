057ed3ecae50142975050bea38558aeaaa726410
Sergey Sova <s.sova@redmadrobot.com>
2020-05-12 17:30:21 +0300

    refactor(swagg): move parser to highway source

--- swagg/src/highway/mod.rs
+++ swagg/src/highway/mod.rs
@@ -4,7 +4,7 @@
 //! Useful when converting OpenAPI structures to printer structures
 
 use indexmap::IndexMap;
-use openapiv3::ReferenceOr;
+use openapiv3::{ReferenceOr, Schema};
 
 /// List of components ready to be printed
 #[derive(Debug, Default)]
@@ -79,6 +79,24 @@ impl Components {
 
     fn insert_request_body(&mut self, body: RequestBody) {
         self.request_bodies.insert(body.name.clone(), body);
+    }
+
+    fn insert_schema(&mut self, component: Component) {
+        self.schemas.insert(component.name.clone(), component);
+    }
+
+    pub fn parse_schema(
+        &mut self,
+        name: &String,
+        schema: &ReferenceOr<Schema>,
+    ) -> Result<(), ParseSchemaError> {
+        let (_, created_components) = parse_ref_or_schema(&name, &schema)?;
+
+        for component in created_components.into_iter() {
+            self.insert_schema(component);
+        }
+
+        Ok(())
     }
 
     // Parse request body from openapiv3 and add it to index
@@ -188,4 +206,105 @@ pub enum ReferenceRelative {
     RequestBody { name: String },
     Schema { name: String },
     Responses { name: String },
+}
+
+#[derive(Debug)]
+pub enum ParseSchemaError {
+    UnsupportedType,
+    ReferenceNotSupported,
+}
+
+fn parse_ref_or_schema(
+    name: &String,
+    ref_or: &ReferenceOr<openapiv3::Schema>,
+) -> Result<(FieldType, Vec<Component>), ParseSchemaError> {
+    match ref_or {
+        ReferenceOr::Item(schema) => parse_schema(&name, &schema),
+        ReferenceOr::Reference { reference } => {
+            log::info!(
+                "reference for schemas is not supported yet. Skipping {} for {}...",
+                reference,
+                name
+            );
+
+            Err(ParseSchemaError::ReferenceNotSupported)
+        }
+    }
+}
+
+fn parse_schema(
+    name: &String,
+    schema: &openapiv3::Schema,
+) -> Result<(FieldType, Vec<Component>), ParseSchemaError> {
+    use inflections::Inflect;
+
+    let mut list = vec![];
+
+    use openapiv3::{SchemaKind, Type};
+
+    match &schema.schema_kind {
+        SchemaKind::Type(schema_type) => {
+            let field_type = match schema_type {
+                Type::Number(_number) => FieldType::Number,
+                Type::Integer(_integer) => FieldType::Integer,
+                Type::String(_string) => FieldType::String,
+                Type::Boolean {} => FieldType::Boolean,
+                Type::Object(object) => {
+                    let (fields, mut created_components) = parse_schema_object(&name, object)?;
+
+                    let component_name = name.clone().to_pascal_case();
+                    let component = Component {
+                        name: component_name.clone(),
+                        description: schema.schema_data.description.clone(),
+                        kind: ComponentKind::Object { fields },
+                    };
+
+                    list.push(component);
+                    list.append(&mut created_components);
+
+                    FieldType::Type(component_name.clone())
+                }
+                Type::Array(_array) => unimplemented!(),
+            };
+
+            Ok((field_type, list))
+        }
+        other => {
+            log::info!("this schema kind is not supported {:?}", other);
+            Err(ParseSchemaError::UnsupportedType)
+        }
+    }
+}
+
+fn parse_schema_object(
+    name: &String,
+    schema_object: &openapiv3::ObjectType,
+) -> Result<(indexmap::IndexMap<String, ComponentField>, Vec<Component>), ParseSchemaError> {
+    use inflections::Inflect;
+
+    let mut components = vec![];
+    let mut fields = indexmap::IndexMap::new();
+
+    for (field_name, schema) in schema_object.properties.iter() {
+        let inner_name = format!("{}{}", name, field_name.clone().to_pascal_case());
+
+        let (field_type, mut created_components) =
+            parse_ref_or_schema(&inner_name, &schema.clone().unbox())?;
+
+        components.append(&mut created_components);
+
+        let field = ComponentField {
+            required: schema_object
+                .required
+                .iter()
+                .find(|found| *found == field_name)
+                .is_some(),
+            description: None, // TODO: parse field description
+            field_type,
+        };
+
+        fields.insert(field_name.clone(), field);
+    }
+
+    Ok((fields, components))
 }
--- swagg/src/lib.rs
+++ swagg/src/lib.rs
@@ -61,14 +61,8 @@ pub fn to_string(source: &str, format: Format) -> Result<String, Error> {
         // }
 
         for (name, schema) in components.schemas.iter() {
-            match parse_ref_or_schema(&name, &schema) {
-                Ok((field, components)) => {
-                    println!("Schema: {} {:#?}", name, field);
-                    println!("Parsed components {:#?}", components);
-                }
-                Err(reason) => {
-                    eprintln!("Failed {} {:#?}", name, reason);
-                }
+            if let Err(reason) = highway_components.parse_schema(&name, &schema) {
+                eprintln!("Failed {} {:#?}", name, reason);
             }
         }
     }
@@ -76,111 +70,4 @@ pub fn to_string(source: &str, format: Format) -> Result<String, Error> {
     println!("{:#?}", highway_components);
 
     Ok(format!("{}", generated.print()))
-}
-
-#[derive(Debug)]
-enum ParseSchemaError {
-    UnsupportedType,
-    ReferenceNotSupported,
-}
-
-fn parse_ref_or_schema(
-    name: &String,
-    ref_or: &ReferenceOr<openapiv3::Schema>,
-) -> Result<(highway::FieldType, Vec<highway::Component>), ParseSchemaError> {
-    match ref_or {
-        ReferenceOr::Item(schema) => parse_schema(&name, &schema),
-        ReferenceOr::Reference { reference } => {
-            log::info!(
-                "reference for schemas is not supported yet. Skipping {} for {}...",
-                reference,
-                name
-            );
-
-            Err(ParseSchemaError::ReferenceNotSupported)
-        }
-    }
-}
-
-fn parse_schema(
-    name: &String,
-    schema: &openapiv3::Schema,
-) -> Result<(highway::FieldType, Vec<highway::Component>), ParseSchemaError> {
-    use inflections::Inflect;
-
-    let mut list = vec![];
-
-    use openapiv3::{SchemaKind, Type};
-
-    match &schema.schema_kind {
-        SchemaKind::Type(schema_type) => {
-            let field_type = match schema_type {
-                Type::Number(_number) => highway::FieldType::Number,
-                Type::Integer(_integer) => highway::FieldType::Integer,
-                Type::String(_string) => highway::FieldType::String,
-                Type::Boolean {} => highway::FieldType::Boolean,
-                Type::Object(object) => {
-                    let (fields, mut created_components) = parse_schema_object(&name, object)?;
-
-                    let component_name = name.clone().to_pascal_case();
-                    let component = highway::Component {
-                        name: component_name.clone(),
-                        description: schema.schema_data.description.clone(),
-                        kind: highway::ComponentKind::Object { fields },
-                    };
-
-                    list.push(component);
-                    list.append(&mut created_components);
-
-                    highway::FieldType::Type(component_name.clone())
-                }
-                Type::Array(_array) => unimplemented!(),
-            };
-
-            Ok((field_type, list))
-        }
-        other => {
-            log::info!("this schema kind is not supported {:?}", other);
-            Err(ParseSchemaError::UnsupportedType)
-        }
-    }
-}
-
-fn parse_schema_object(
-    name: &String,
-    schema_object: &openapiv3::ObjectType,
-) -> Result<
-    (
-        indexmap::IndexMap<String, highway::ComponentField>,
-        Vec<highway::Component>,
-    ),
-    ParseSchemaError,
-> {
-    use inflections::Inflect;
-
-    let mut components = vec![];
-    let mut fields = indexmap::IndexMap::new();
-
-    for (field_name, schema) in schema_object.properties.iter() {
-        let inner_name = format!("{}{}", name, field_name.clone().to_pascal_case());
-
-        let (field_type, mut created_components) =
-            parse_ref_or_schema(&inner_name, &schema.clone().unbox())?;
-
-        components.append(&mut created_components);
-
-        let field = highway::ComponentField {
-            required: schema_object
-                .required
-                .iter()
-                .find(|found| *found == field_name)
-                .is_some(),
-            description: None, // TODO: parse field description
-            field_type,
-        };
-
-        fields.insert(field_name.clone(), field);
-    }
-
-    Ok((fields, components))
 }
